774c08daf46ff5826816fc20cfce0d794319a974
Dawid Stefanko <dawid.stefanko@grupa.itdesk.eu>
2021-05-08 17:38:40 +0200

    Added axios globally

--- src/main.js
+++ src/main.js
@@ -4,10 +4,13 @@ import './registerServiceWorker';
 import router from './router';
 import store from './store';
 import CustomComponent from '@/components/CustomComponent.vue';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 createApp(App)
    .use(store)
    .use(router)
+   .use(VueAxios, axios)
    .component('CustomComponent', CustomComponent)
    .directive('colorful', {
       beforeMount(el) {
--- src/views/Request.vue
+++ src/views/Request.vue
@@ -3,19 +3,17 @@
 </template>
 
 <script>
-const axios = require('axios');
 export default {
    data() {
       return {
          data: null,
-         id: null,
       };
    },
 
    mounted() {
-      axios
+      this.axios
          .get('https://api.coindesk.com/v1/bpi/currentprice.json')
-         .then(response => (this.info = response));
+         .then(response => (this.data = response.data));
 
       // fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
       //    .then(response => response.json())
